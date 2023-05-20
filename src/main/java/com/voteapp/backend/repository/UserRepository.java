1cbf65d096a55fb10dd6c35e1ef0e80e385dd7b1
xiaoxuan0117 <xiaoxuanpan0117@gmail.com>
2023-05-20 17:05:24 +0800

    fix: remove unnecessary import

--- src/main/java/com/voteapp/backend/repository/UserRepository.java
+++ src/main/java/com/voteapp/backend/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.voteapp.backend.repository;
 import com.voteapp.backend.modal.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
